eafdc7a82aa0f3e1dcf9cd385e7ca8c6bc7bd627
RenanOD <renan_oclides@hotmail.com>
2015-05-05 01:28:32 -0300

    Funções de impressão de erro e tempo foram separadas

--- testG.jl
+++ testG.jl
@@ -1,19 +1,6 @@
 include ("TR.jl")
 include ("SR.jl")
-
-function printError (integral, f, g, spacing, intervals)
-
-  for i=[1:size(intervals,1)]
-    a = intervals [i,1]
-    b = intervals [i,2]
-    println ("\n between $a and $b :\n")
-    for i=[1:length(spacing)]
-      n = spacing[i]
-      error = integral(a, b) - g(f, a, b, n)
-      println (" Error (n = $n): $error")
-    end
-  end
-end
+include ("printError.jl")
 
 spacing = [2, 10, 100, 1000]
 intervals = [0 1; 0 10; -1 1; -10 10]
--- timeG.jl
+++ timeG.jl
@@ -1,20 +1,7 @@
 include ("TR.jl")
 include ("SR.jl")
 using TimeIt
-
-function printError (f, g, spacing, intervals)
-
-  for i=1:1:size(intervals,1)
-    a = intervals [i,1]
-    b = intervals [i,2]
-    println ("\n between $a and $b :\n")
-    for i=[1:length(spacing)]
-      n = spacing[i]
-      println (" Time (n = $n): ")
-      @timeit g(f, a, b, n)
-    end
-  end
-end
+include ("printTime.jl")
 
 spacing = [2, 10, 100, 1000]
 intervals = [0 1; 0 10; -1 1; -10 10]
@@ -22,26 +9,26 @@ for g = [TR, SR]
 
   f(x) = 1
   println ("\n \n Time for f(x) = 1 ($g) \n")
-  printError (f, g, spacing, intervals)
+  printTime (f, g, spacing, intervals)
    
   f(x) = x
   println ("\n \n Time for f(x) = x ($g) \n")
-  printError (f, g, spacing, intervals)
+  printTime (f, g, spacing, intervals)
 
   f(x) = x^2
   println ("\n \n Time for f(x) = x^2 ($g) \n")
-  printError (f, g, spacing, intervals)
+  printTime (f, g, spacing, intervals)
 
   f(x) = x^3
   println ("\n \n Time for f(x) = x^3 ($g) \n")
-  printError (f, g, spacing, intervals)
+  printTime (f, g, spacing, intervals)
 
   f(x) = x^4
   println ("\n \n Time for f(x) = x^4 ($g) \n")
-  printError (f, g, spacing, intervals)
+  printTime (f, g, spacing, intervals)
 
   f(x) = e^x
   println ("\n \n Time for f(x) = e^x ($g) \n")
-  printError (f, g, spacing, intervals)
+  printTime (f, g, spacing, intervals)
 
 end
