4f7c9efe6b556fda561e94df5bb10e0275440db5
RenanOD <renan_oclides@hotmail.com>
2015-05-22 07:09:56 -0300

    Novas funções

--- getAprox.jl
+++ getAprox.jl
@@ -1,16 +1,16 @@
 include ("difdiv.jl")
 
-function getAprox(x, X, Y)
+function getAprox(point, X, Y)
 
   C=difdiv(X,Y)
   j=length(X)
   p=C[1]
 
   for i=[1:j-1]
-    difX=x-X[1]
+    difX=point-X[1]
 
     for k=[2:i]
-      difX*=(x-X[k])
+      difX*=(point-X[k])
     end
 
   p+=difX*C[i+1]
