521dd0d69c6c6d2c2366eb00b47f004df8744786
RenanOD <renan_oclides@hotmail.com>
2015-07-20 23:41:17 -0300

    Improper

--- sinh_tanh/printError_sinh_tanh.jl
+++ sinh_tanh/printError_sinh_tanh.jl
@@ -4,6 +4,6 @@ function printError_sinh_tanh(f,a,b,integral,n)
   approx=sinh_tanh(f,a,b,n);
   error=exact-approx;
 
-  println ("in the interval between $a e $b: $error")
+  println ("in the interval between $a and $b: $error")
 
 end
