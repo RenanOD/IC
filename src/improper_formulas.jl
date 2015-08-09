09d330a8855c1326487c69fc717f8f450e3732fd
RenanOD <renan_oclides@hotmail.com>
2015-08-09 20:09:40 -0300

    from '=[a:b]' to '=a:b'

--- src/improper_formulas.jl
+++ src/improper_formulas.jl
@@ -11,7 +11,7 @@ h=4.5/2.0^n
 x(k)=e^(pi*sinh(k*h))
 w(k)=x(k)*pi*cosh(k*h)
 
-for k=[-s:s-1]
+for k=-s:s-1
   approx+=(f(x(k))*w(k)+f(x(k+1))*w(k+1))*h/2
 end
 return approx
@@ -25,7 +25,7 @@ h=4.5/2.0^n
 x(k)=sinh(pi*sinh(k*h)/2)
 w(k)=cosh(pi*sinh(k*h)/2)*pi*cosh(k*h)/2
 
-for k=[-s:s-1]
+for k=-s:s-1
   approx+=(f(x(k))*w(k)+f(x(k+1))*w(k+1))*h/2
 end
 return approx
@@ -41,7 +41,7 @@ end
 function mid_point(f,a,b,n)
 h=(b-a)/n
 sum=f(a+h/2)
-for i=[1:n-1]
+for i=1:n-1
   sum+=f(a+h/2+i*h)
 end
 approx=h*sum
