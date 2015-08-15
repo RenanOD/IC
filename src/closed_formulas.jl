7fcea41234d50c20114957d84d899d0242e60a7d
RenanOD <renan_oclides@hotmail.com>
2015-08-15 14:46:17 -0300

    indentation update

--- src/closed_formulas.jl
+++ src/closed_formulas.jl
@@ -2,22 +2,22 @@ export simpsons_rule
 export trapezoidal_rule
 
 function simpsons_rule(f, a, b, n)
-  if n%2!=0
+  if n%2 != 0
     n += 1
   end
-  h = (b-a)/n
-  approx = (f(a) + f(b) + 4f(a+h))
-  for i=2:2:n-1
+  h = (b - a)/n
+  approx = (f(a) + f(b) + 4f(a + h))
+  for i = 2:2:n-1
     x = a + i*h
-    approx += 2f(x) + 4f(x+h)
+    approx += 2f(x) + 4f(x + h)
   end
   return approx*(h/3)
 end
 
 function trapezoidal_rule(f, a, b, n)
-  h = (b-a)/n
+  h = (b - a)/n
   approx = (f(a) + f(b))/2
-  for i=1:n-1
+  for i = 1:n-1
     x = a + i*h
     approx += f(x)
   end
--- test/test_sinh_tanh.jl
+++ test/test_sinh_tanh.jl
@@ -1,34 +1,34 @@
-function test_precision(g,a,b,exact)
-  approx=sinh_tanh(g,a,b,12)
-  @test_approx_eq_eps(exact,approx,1e-6)
+function test_precision(g, a, b, exact)
+  approx=sinh_tanh(g, a, b, 12)
+  @test_approx_eq_eps(exact, approx, 1e-6)
 end
 
 function test_sinh_tanh()
 
-g(t)=t*log(1+t)
-(a,b)=(0,1)
-exact=1/4
-test_precision(g,a,b,exact)
+  g(t) = t*log(1 + t)
+  (a, b) = (0, 1)
+  exact = 1/4
+  test_precision(g, a, b, exact)
 
-g(t)=t^2*atan(t)
-(a,b)=(0,1)
-exact=(pi-2+2log(2))/12
-test_precision(g,a,b,exact)
+  g(t) = t^2*atan(t)
+  (a, b) = (0, 1)
+  exact = (pi - 2 + 2log(2))/12
+  test_precision(g, a, b, exact)
 
-g(t)=e^t*cos(t)
-(a,b)=(0,pi/2)
-exact=(e^(pi/2)-1)/2
-test_precision(g,a,b,exact)
+  g(t) = e^t*cos(t)
+  (a, b) = (0, pi/2)
+  exact = (e^(pi/2) - 1)/2
+  test_precision(g, a, b, exact)
 
-g(t)=atan(sqrt(2+t^2))/((1+t^2)*sqrt(2+t^2))
-(a,b)=(0,1)
-exact=5*pi^2/96
-test_precision(g,a,b,exact)
+  g(t) = atan(sqrt(2 + t^2))/((1 + t^2)*sqrt(2 + t^2))
+  (a, b) = (0, 1)
+  exact = 5*pi^2/96
+  test_precision(g, a, b, exact)
 
-g(t)=sqrt(t)/sqrt(1-t^2)
-(a,b)=(0,1)
-exact=2*sqrt(pi)*gamma(3/4)/gamma(1/4)
-test_precision(g,a,b,exact)
+  g(t) = sqrt(t)/sqrt(1 - t^2)
+  (a, b) = (0, 1)
+  exact = 2*sqrt(pi)*gamma(3/4)/gamma(1/4)
+  test_precision(g, a, b, exact)
 
 end
 
