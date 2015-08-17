b0aab077b4b836a90738279db566d44a1b2cc50b
RenanOD <renan_oclides@hotmail.com>
2015-08-16 22:03:55 -0300

    sinh_tanh update

--- src/sinh_tanh.jl
+++ src/sinh_tanh.jl
@@ -1,22 +1,24 @@
 export sinh_tanh
 
 function sinh_tanh(f, a, b, n)
-  s = 2.0^n
-  h = 4.5/2^n
-  q(k) = e^(-2*sinh(k*h))
+  h = 1.0/2^n
+  q(k) = e^(-2*sinh(k))
   subs(k) = (b - a)*q(k)/(1 + q(k))
-  g(t) = 2*(b - a)*t
-  w_k(k) = h*q(k)*cosh(k*h)/(1 + q(k))^2
-  x_k(k) = tanh(sinh(k*h))
+  g(k) = 2*(b - a)*h*q(k)*cosh(k)/(1 + q(k))^2
+  i = 1
 
-  approx = f((a+b)/2)*g(w_k(0))
+  approx = f((a+b)/2)*g(0)
 
-  for k = 1:s
+  for k = h:h:4
     j = subs(k)
     if j < 1e-16
-      j = 1e-16
+      j = BigFloat(subs(k))
     end
-    approx += f(a + j)*g(w_k(k)) + f(b - j)*g(w_k(k))
+    f1 = f(a + j)
+    f2 = f(b - j)
+
+    dxdt = g(k)
+    approx += (f1 + f2)*dxdt
   end
 return approx
 end
