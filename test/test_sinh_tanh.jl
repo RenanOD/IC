b3712c682c2a2cbbb85c05cbea3ae8950fa97a0b
RenanOD <renan_oclides@hotmail.com>
2015-08-22 13:47:32 -0300

    the limits of the integral must be given in float

--- test/test_sinh_tanh.jl
+++ test/test_sinh_tanh.jl
@@ -6,12 +6,12 @@ end
 function test_sinh_tanh()
 
   g(t) = t*log(1 + t)
-  (a, b) = (0, 1)
+  (a, b) = (0, 1.0)
   exact = 1/4
   test_precision(g, a, b, exact)
 
   g(t) = t^2*atan(t)
-  (a, b) = (0, 1)
+  (a, b) = (0, 1.0)
   exact = (pi - 2 + 2log(2))/12
   test_precision(g, a, b, exact)
 
@@ -21,12 +21,12 @@ function test_sinh_tanh()
   test_precision(g, a, b, exact)
 
   g(t) = atan(sqrt(2 + t^2))/((1 + t^2)*sqrt(2 + t^2))
-  (a, b) = (0, 1)
+  (a, b) = (0, 1.0)
   exact = 5*pi^2/96
   test_precision(g, a, b, exact)
 
   g(t) = sqrt(t)/sqrt(1 - t^2)
-  (a, b) = (0, 1)
+  (a, b) = (0, 1.0)
   exact = 2*sqrt(pi)*gamma(3/4)/gamma(1/4)
   test_precision(g, a, b, exact)
 
