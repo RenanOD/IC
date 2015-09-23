49c1fb60bbeae35c55901797afa8d2d52d78f9b6
RenanOD <renan_oclides@hotmail.com>
2015-09-22 23:36:14 -0300

    small change

--- src/improper_formulas.jl
+++ src/improper_formulas.jl
@@ -19,12 +19,11 @@ end
 
 function double_inf(f; n = 10)
   approx = 0
-  s = 2.0^n
-  h = 4.5/2.0^n
+  h = 1.0/2.0^n
   x(k) = sinh(pi*sinh(k*h)/2)
   w(k) = cosh(pi*sinh(k*h)/2)*pi*cosh(k*h)/2
 
-  for k = -s:s-1
+  for k = -4.8:h:4.8
     approx += (f(x(k))*w(k) + f(x(k + 1))*w(k + 1))*h/2
   end
   return approx
