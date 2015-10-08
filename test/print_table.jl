8114bc63fe7da24b238f94444c106d540039f0b7
RenanOD <renan_oclides@hotmail.com>
2015-10-08 02:15:03 -0300

    ...

--- test/print_table.jl
+++ test/print_table.jl
@@ -1,9 +1,11 @@
 using NumInt
 
-function print_easy(f, a, b, exact; Nf = [2^4, 2^5, 2^6, 2^7, 2^8, 2^9, 2^10, 2^11, 2^12])
+function print_easy(f, a, b, exact)
   
+  Nf = [2^4, 2^5, 2^6, 2^7, 2^8, 2^9, 2^10, 2^11, 2^12]
   u = size(Nf, 1); k = 1;
-  functions = [trapezoidal_rule, simpsons_rule, mid_point, sinh_tanh]
+  functions = [trapezoidal_rule, simpsons_rule, mid_point, sinh_tanh, open];
+  t = size(functions, 1);
   
   tabela = cell(u + 1, 5)
   tabela[1,1] = "Nf"
@@ -13,7 +15,7 @@ function print_easy(f, a, b, exact; Nf = [2^4, 2^5, 2^6, 2^7, 2^8, 2^9, 2^10, 2^
     tabela[i + 1,1] = Nf[i];
   end
   
-  for k = 1:4
+  for k = 1:t
     for i = 2:1:u + 1
       g = functions[k]
       tabela[i, k + 1] = abs(exact - g(f, a, b, Nf = Nf[i - 1]))
