2b9391ca5799aaa03d2fd7af2e68335e6163506f
RenanOD <renan_oclides@hotmail.com>
2015-10-16 12:25:22 -0300

    gaussian quadrature error

--- src/NumInt.jl
+++ src/NumInt.jl
@@ -4,7 +4,6 @@ include("closed_formulas.jl")
 include("simpsons_rule_adapted.jl")
 include("improper_formulas.jl")
 include("sinh_tanh.jl")
-include("gaussian_quadrature.jl")
 include ("open_formulas.jl")
 
 end
