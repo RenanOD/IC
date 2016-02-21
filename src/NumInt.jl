6a9579f8a283f864bddafabcdf85a5155d69141d
RenanOD <renan_oclides@hotmail.com>
2016-02-20 23:56:26 -0200

    Added Romberg method

--- src/NumInt.jl
+++ src/NumInt.jl
@@ -3,7 +3,7 @@ module NumInt
 Pkg.clone("https://github.com/ajt60gaibb/FastGaussQuadrature.jl")
 using FastGaussQuadrature
 
-include("closed_formulas.jl")
+include("newton_formulas.jl")
 include("gauss_formulas.jl")
 include("adaptive_formulas.jl")
 include("improper_formulas.jl")
